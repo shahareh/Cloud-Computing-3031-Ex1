f5e55e52a95929df73318d76f510b57b70fb32bf
shahareh <68902894+shahareh@users.noreply.github.com>
2023-07-31 00:37:08 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,101 @@
+from flask import Flask, request, jsonify
+import random
+from datetime import datetime
+import math
+
+
+app = Flask(__name__)
+
+
+# In-memory data store (for simplicity, we'll use a Python list)
+parking_lots_db = {}
+parking_plates_entered = []
+parking_lots_in_use = []
+generated_ticket_ids = []
+
+def generate_unique_ticket_id():
+    while True:
+        # Generate a random 4-digit number between 1000 and 9999 (inclusive)
+        number = random.randint(1000, 9999)
+        
+        # Check if the number is not in the list of existing numbers
+        if number not in generated_ticket_ids:
+            generated_ticket_ids.append(number)
+            return number
+
+
+
+def save_parking_details(plate, parking_lot,ticket_id):
+    
+    # Save the entry data in the data_store (you can add more logic here)
+    parking_lots_db[ticket_id] = {
+        "entry_time" : datetime.now(),
+        "plate" : plate,
+        "parking_lot" : parking_lot
+    }
+
+    parking_plates_entered.append(plate)
+    parking_lots_in_use.append(parking_lot)
+
+
+def get_parking_details_by_ticket_id(ticket_id):
+    parking_details = parking_lots_db[ticket_id]
+   
+    parking_plates_entered.remove(parking_details['plate'])
+    parking_lots_in_use.remove(parking_details['parking_lot'])
+    del parking_lots_db[ticket_id]
+
+    return parking_details
+    
+
+def calculate_parking_price(parking_total_minutes):
+    parking_occurrences = math.ceil(parking_total_minutes / 15)
+    price_per_hour = 10.0
+    return parking_occurrences * (price_per_hour / 4)
+
+
+@app.route('/entry', methods=['POST'])
+def entry():
+    plate = request.args.get('plate', '')
+    parking_lot = request.args.get('parkingLot', '')
+    
+    if plate in parking_plates_entered:
+        return "This parking lot is already in use.", 422
+    
+    if parking_lot in parking_lots_in_use:
+        return "A Car with the same plate is already inside the parking area.", 422
+
+    
+    ticket_id = generate_unique_ticket_id()
+    save_parking_details(plate, parking_lot,ticket_id)
+    
+    return jsonify({"ticketId": ticket_id}), 201
+
+
+
+@app.route('/exit', methods=['POST'])
+def exit():
+    ticket_id = request.args.get('ticketId', '')
+    try:
+        ticket_id = int(ticket_id)
+
+    except ValueError:
+        return "Invalid TicketId. Please provide a valid integer value.", 422
+
+    if ticket_id not in parking_lots_db:
+        return "TicketId is not exist.", 404
+    
+    parking_details = get_parking_details_by_ticket_id(ticket_id)
+    parking_total_time = (datetime.now() - parking_details['entry_time']).total_seconds() / 60
+    charge = calculate_parking_price(parking_total_time)
+
+    return jsonify({
+        'license_plate': parking_details['plate'],
+        'parking_lot': parking_details['parking_lot'],
+        'parking_time': parking_total_time,
+        'charge': charge
+    }), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ deploy-parking-app.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Set variables
+STACK_NAME=parking-webapi-app
+REGION=eu-west-1
+
+# Define CloudFormation template
+read -r -d '' TEMPLATE << EOM
+{
+    "AWSTemplateFormatVersion": "2010-09-09",
+    "Resources": {
+        "EC2Instance": {
+            "Type": "AWS::EC2::Instance",
+            "Properties": {
+                "ImageId": "ami-01dd271720c1ba44f",
+                "InstanceType": "t2.micro",
+                "SecurityGroups": [
+                    {
+                        "Ref": "InstanceSecurityGroup"
+                    }
+                ],
+                "UserData": {
+                    "Fn::Base64": {
+                        "Fn::Join": [
+                            "",
+                            [
+                                "#!/bin/bash\n",
+                                "# Update package lists\n",
+                                "apt-get update\n",
+                                "# Install dependencies\n",
+                                "apt-get install -y git python3 python3-flask\n",
+                                "# Clone repository\n",
+                                "git clone https://github.com/shahareh/Cloud-Computing-3031.git\n",
+                                "# Run Flask app\n",
+                                "cd your-repo\n",
+                                "export FLASK_APP=app.py\n",
+                                "nohup flask run --host=0.0.0.0 &"
+                            ]
+                        ]
+                    }
+                }
+            }
+        },
+        "InstanceSecurityGroup": {
+			"Type": "AWS::EC2::SecurityGroup",
+			"Properties": {
+				"GroupDescription": "Enable HTTP access on the inbound port",
+				"SecurityGroupIngress": [
+					{
+						"IpProtocol": "tcp",
+						"FromPort": 5000,
+						"ToPort": 5000,
+						"CidrIp": "0.0.0.0/0"
+					}
+				]
+			}
+		}
+    }
+}
+
+EOM
+
+# Create CloudFormation stack
+aws cloudformation create-stack --stack-name $STACK_NAME --template-body "$TEMPLATE" --region $REGION
+
+# Wait for stack creation to complete
+aws cloudformation wait stack-create-complete --stack-name $STACK_NAME --region $REGION
+
+# Get public IP of EC2 instance
+INSTANCE_ID=$(aws cloudformation describe-stack-resources --stack-name $STACK_NAME --region $REGION --query "StackResources[?LogicalResourceId=='EC2Instance'].PhysicalResourceId" --output text)
+PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --region $REGION --query "Reservations[].Instances[].PublicIpAddress" --output text)
+
+# Output public IP
+echo "Public IP: $PUBLIC_IP"
